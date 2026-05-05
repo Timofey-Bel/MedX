67423c80329cb1af997b1ea4538ed4553da26132
Timofey <betise2007@inbox.ru>
2026-05-05 14:00:00 +0300

    Update styles for laptop v1.2

--- resources/views/partials/main_mobile_menu.blade.php
+++ resources/views/partials/main_mobile_menu.blade.php
@@ -39,39 +39,43 @@
                     <span class="nav-text">Новости</span>
                 </a>
             </div>
-            <div class="nav-item settings">
-                <a href="{{ url('/main_settings') }}">
+            <div class="bottom-section">
+                <div class="nav-item settings">
+                   <a href="{{ url('/main_settings') }}">
                     <div class="nav-icon settings-icon"></div>
                     <span class="nav-text">Настройки</span>
-                </a>
-            </div>
-        </nav>
-    </div>
-
-    <div class="user-section">
-        <div class="user-avatar">
-            <a href="{{ url('/profile') }}">
-                @if(Auth::user()->avatar_url)
-                    <svg width="50" height="50" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <defs>
+                    </a>
+                </div>
+                <div class="user-section">
+                    <div class="user-avatar">
+                        <a href="{{ url('/profile') }}">
+                        @if(Auth::user()->avatar_url)
+                            <svg width="50" height="50" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <defs>
                             <clipPath id="avatarClipMobile">
                                 <circle cx="40" cy="40" r="40"/>
                             </clipPath>
-                        </defs>
-                        <image href="{{ Auth::user()->avatar_url }}" width="80" height="80" clip-path="url(#avatarClipMobile)" preserveAspectRatio="xMidYMid slice"/>
-                    </svg>
-                @else
-                    <svg width="50" height="50" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            </defs>
+                            <image href="{{ Auth::user()->avatar_url }}" width="80" height="80" clip-path="url(#avatarClipMobile)" preserveAspectRatio="xMidYMid slice"/>
+                            </svg>
+                        @else
+                        <svg width="50" height="50" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="40" cy="40" r="40" fill="#3B97A2"/>
                         <text x="40" y="48" font-family="Involve-Medium, Helvetica" font-size="42" font-weight="500" fill="white" text-anchor="middle" dominant-baseline="middle">{{ Auth::user()->avatar_letter }}</text>
-                    </svg>
-                @endif
-            </a>
-        </div>
-        <div class="user-info">
-            <div class="user-name">{{ Auth::user()->display_name }}</div>
-            <div class="user-email">{{ Auth::user()->email }}</div>
-        </div>
+                        </svg>
+                        @endif
+                        </a>
+                    </div>
+                    <div class="user-info">
+                        <div class="user-name">{{ Auth::user()->display_name }}</div>
+                        <div class="user-email">{{ Auth::user()->email }}</div>
+                   </div>
+                </div>
+            </div>
+
+        </nav>
     </div>
+
+    
 
 </nav>
